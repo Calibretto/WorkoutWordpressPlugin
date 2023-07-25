bcd635dc12ea0711790f960e367bed475b96de1a
Brian Hackett <brian.a.hackett@gmail.com>
2023-07-25 18:20:38 +0100

    Added basic plugin header

---
+++ workout.php
@@ -0,0 +1,10 @@
+<?php
+
+/*
+ * Plugin Name:       Workout Tracker
+ * Description:       Assign and track workouts.
+ * Version:           0.0.1
+ * Author:            Brian Hackett
+ * Author URI:        https://www.brianhackett.co.uk
+ */
+?>
